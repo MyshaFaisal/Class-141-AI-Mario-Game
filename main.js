c449271b733b84af1f556517c54cd9ec95c2fa85
MyshaFaisal <88610106+MyshaFaisal@users.noreply.github.com>
2023-04-03 15:27:05 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,60 @@
+function preload() {
+	world_start = loadSound("world_start.wav");
+	setSprites();
+	MarioAnimation();
+}
+
+function setup() {
+	canvas = createCanvas(1240,336);
+	canvas.parent('canvas');
+	
+	instializeInSetup(mario);
+
+	video = createCanvas(VIDEO);
+	video.size(800, 400);
+	video.parent('game_console');
+
+	poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+	console.log('Model Loaded!');
+  }
+  
+function gotPoses(results)
+  {
+	if(results.length > 0)
+	{
+	  console.log(results);
+	  noseX = results[0].pose.nose.x;
+	  noseY = results[0].pose.nose.y;
+	}
+  }
+
+  noseX = "";
+  noseY = "";
+  GameStatus = "";
+
+function startGame()
+{
+	GameStatus = "start";
+	document.getElementById("status").innerHTML = "Game Is Loading";
+}
+
+function game()
+{
+	console.log("noseX = " + noseX +",noseY = "+ noseY);
+	initializeInDraw();
+	moveEnvironment(mario);
+	drawSprites();
+}
+function draw() {
+	game()
+}
+
+
+
+
+
+
